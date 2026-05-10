b2bb80791e04dc18ddc3e757381b4e345d863082
heikoduemig <43315579+heikoduemig@users.noreply.github.com>
2026-05-10 18:50:42 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME='route66-after-dark-wow-v1';
-const APP_SHELL=['./','./index.html','./adultData.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET') return;
- e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>caches.match('./index.html'))));
-});
